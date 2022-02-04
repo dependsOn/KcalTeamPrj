efa08033ac377fec36ecb444388ba8f4e5b62cd6
YooJeong <YooJeong@DESKTOP-RJPECNM>
2022-02-05 02:17:42 +0900

    Merge branch 'develop' of https://github.com/dependsOn/KcalTeamPrj.git into develop

---
+++ KcalTeamPrj/src/main/java/vo/MealcardVO.java
@@ -0,0 +1,55 @@
+package vo;
+
+public class MealcardVO {
+	private int mealnum;
+	private int unum;
+	private String date;
+	private String time;
+	private int totalKcal;
+	
+	public MealcardVO() {
+
+	}
+
+	public int getMealnum() {
+		return mealnum;
+	}
+
+	public void setMealnum(int mealnum) {
+		this.mealnum = mealnum;
+	}
+
+	public int getUnum() {
+		return unum;
+	}
+
+	public void setUnum(int unum) {
+		this.unum = unum;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public int getTotalKcal() {
+		return totalKcal;
+	}
+
+	public void setTotalKcal(int totalKcal) {
+		this.totalKcal = totalKcal;
+	}
+	
+	
+}
